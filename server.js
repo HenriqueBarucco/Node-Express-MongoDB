0ad4690cc07abdaf9bb927e65e2f703854bbf048
Henrique Barucco <henrique_barucco@hotmail.com>
2022-12-27 20:31:56 -0300

    Init

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'Listagem de autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 })
 
 server.listen(port, () => {
